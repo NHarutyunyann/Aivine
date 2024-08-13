21aa6f1a46f5188c3c0f97dc2f61d5b9680b0881
N-Harutyunayn <12.Harutyunyan@mail.ru>
2024-08-14 02:05:59 +0400

    contact blade

--- resources/views/front/contact.blade.php
+++ resources/views/front/contact.blade.php
@@ -8,7 +8,34 @@
 @section('style') @endsection
 @section('content')
 
-<h1>Contact</h1>
+<div class='contact'>
+    <div class='contact_first'>
+        <img class="contact_first_img" src="/images/aivine/contactimage.svg" alt="images">
+        <p class="contact_first_text">Fill in your information</p>
+    </div>
+
+    <div class="send_message">
+        <div class="send_message_info">
+            <p class="send_message_title">Fill in your information, and we will contact you.</p>
+        </div>
+        <form action="/send_message" method="POST">
+            @csrf
+            <div class="send_message_inputs">
+                <div class="name_phone">
+                    <input name="name" type="text" placeholder="Full Name *">
+                    <input name="phone_number" type="text" placeholder="Phone Number *">
+                </div>
+                <div class="message">
+                    <textarea name="message" placeholder="Message" cols="30" rows="10"></textarea>
+                </div>
+            </div>
+            <div class="send_message_btn">
+                <button>Order</button>
+            </div>
+        </form>
+
+    </div>
+</div>
 
 
 
