c7b6fdaca18ef2a714595e0dbb4a060837e2f91b
N-Harutyunayn <12.Harutyunyan@mail.ru>
2024-08-20 18:34:59 +0400

    dasa

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1,212 +1,239 @@
 @extends('layouts.front')
-@section('title')Aivine @endsection
-@section('description') @endsection
-@section('keywords') @endsection
+@section('title')
+    Aivine
+@endsection
+@section('description')
+@endsection
+@section('keywords')
+@endsection
 @section('url')
     {{ url('/') }}
 @endsection
-@section('style') @endsection
+@section('style')
+@endsection
 
 @section('content')
-   
-{{-- 
-<div class='home'>
-    <div class='homeblock1'>
-        <img class='home_first_image' src="/images/aivine/homeFirstImage.png" alt="images" >
-        <p class="home_first_text">Thermolysis - Galvanic Blend Electrolysis Permanent, painless Hair Removal System</p>
-        <button class="home_first_btn">ORDER</button>
-    </div>
-</div> --}}
-
-
-<div class="home_second">
-    <div class="home_second_title">
-        <h2>AIVINE SYSTEM INCLUDES</h2>
-    </div>
-    <div class="home_second_boxes">
-        <div class="home_second_box">
-            <div class="home_second_image">
-                <img src="/images/aivine/second.png" alt="images">
-            </div>
-            <div class="home_second_info">
-                <h4 class="home_second_info_title">A PROBE STYLUS</h4>
-                <p class="home_second_info_text">Aivine is a box that is 28cm wide and 21.5 in length.</p>
-            </div>
-        </div>
-        <div class="home_second_box">
-            <div class="home_second_image">
-                <img src="/images/aivine/second.png" alt="images">
-            </div>
-            <div class="home_second_info">
-                <h4 class="home_second_info_title">A PROBE STYLUS</h4>
-                <p class="home_second_info_text">Aivine is a box that is 28cm wide and 21.5 in length.</p>
-            </div>
-        </div>
-        <div class="home_second_box">
-            <div class="home_second_image">
-                <img src="/images/aivine/second.png" alt="images">
-            </div>
-            <div class="home_second_info">
-                <h4 class="home_second_info_title">A PROBE STYLUS</h4>
-                <p class="home_second_info_text">Aivine is a box that is 28cm wide and 21.5 in length.</p>
-            </div>
-        </div>
-        <div class="home_second_box">
-            <div class="home_second_image">
-                <img src="/images/aivine/second.png" alt="images">
-            </div>
-            <div class="home_second_info">
-                <h4 class="home_second_info_title">A PROBE STYLUS</h4>
-                <p class="home_second_info_text">Aivine is a box that is 28cm wide and 21.5 in length.</p>
-            </div>
-        </div>
-        <div class="home_second_box">
-            <div class="home_second_image">
-                <img src="/images/aivine/second.png" alt="images">
-            </div>
-            <div class="home_second_info">
-                <h4 class="home_second_info_title">A PROBE STYLUS</h4>
-                <p class="home_second_info_text">Aivine is a box that is 28cm wide and 21.5 in length.</p>
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class="home_third">
-    <div class="home_third_title">
-        <h2>General view of the AIVINE equipment</h2>
-        <p>PRICE 18.000$</p>
-    </div>
-    <div class="home_third_image">
-        <img src="/images/aivine/third.png" alt="images">
-    </div>
-</div>
-
-
-<div class="home_fourth">
-    <div class="home_fourth_title">
-        <h2>We care not only about hair removal, but also your safety, comfort, and beauty.</h2>
-    </div>
-    <div class="home_fourth_boxes">
-        <div class="home_fourt_box">
-            <div class="home_fourt_box_img">
-                <img src="/images/aivine/fourt.png" alt="images">
-            </div>
-            <div class="home_fourt_box_info">
-                <div class="home_fourt_box_info_text">
-                    <h3>How Many Treatments Does It Take?</h3>
-                    <p>Many hairs will be eliminated with only one treatment, but some will need two or more treatments to achieve permanency. Electrolysis involves a series of treatments over a period of 1-3 months. The overall length of time depends on the amount of hair, its coarseness, and the cause of the excess hair, but once the dermal papilla has been eliminated, the hair is dead and will not grow again.</p>
+    <div class="home">
+        <div class='contact_first'>
+            <img class="contact_first_img" src="/images/aivine/homefirstimg.svg" alt="images">
+            <div class="contact_first_text">
+                <p class="">Thermolysis - Galvanic Blend Electrolysis Permanent, painless Hair Removal System</p>
+                <button class="home_first_btn">ORDER</button>
+               
+            </div> 
+            <div class="home_first_2">
+                <img class="contact_first2_img" src="/images/aivine/first2.png" alt="images">
+                <div class="home_first_2_info">
+                    <p class="home_first_2_info_text">We offer a perfect needle hair removal systems that will produce 100%
+                        results and help you grow the number of customers. Outstanding for a professional salon and Medispa
+                        setting.</p>
+                    <div class="home_first_2_info_text_name">-<p>AIVINE Beauty & ESTHETICS</p>-</div>
                 </div>
-                <div class="home_fourt_box_aivine">
-                    <p>- AIVINE Beauty & ESTHETICS -</p>
+            </div>
+          
+        </div>
+
+      
+
+        <div class="home_second">
+            <div class="home_second_title">
+                <h2>AIVINE SYSTEM INCLUDES</h2>
+            </div>
+            <div class="home_second_boxes">
+                <div class="home_second_box">
+                    <div class="home_second_image">
+                        <img src="/images/aivine/second.png" alt="images">
+                    </div>
+                    <div class="home_second_info">
+                        <h4 class="home_second_info_title">A PROBE STYLUS</h4>
+                        <p class="home_second_info_text">Aivine is a box that is 28cm wide and 21.5 in length.</p>
+                    </div>
+                </div>
+                <div class="home_second_box">
+                    <div class="home_second_image">
+                        <img src="/images/aivine/second.png" alt="images">
+                    </div>
+                    <div class="home_second_info">
+                        <h4 class="home_second_info_title">A PROBE STYLUS</h4>
+                        <p class="home_second_info_text">Aivine is a box that is 28cm wide and 21.5 in length.</p>
+                    </div>
+                </div>
+                <div class="home_second_box">
+                    <div class="home_second_image">
+                        <img src="/images/aivine/second.png" alt="images">
+                    </div>
+                    <div class="home_second_info">
+                        <h4 class="home_second_info_title">A PROBE STYLUS</h4>
+                        <p class="home_second_info_text">Aivine is a box that is 28cm wide and 21.5 in length.</p>
+                    </div>
+                </div>
+                <div class="home_second_box">
+                    <div class="home_second_image">
+                        <img src="/images/aivine/second.png" alt="images">
+                    </div>
+                    <div class="home_second_info">
+                        <h4 class="home_second_info_title">A PROBE STYLUS</h4>
+                        <p class="home_second_info_text">Aivine is a box that is 28cm wide and 21.5 in length.</p>
+                    </div>
+                </div>
+                <div class="home_second_box">
+                    <div class="home_second_image">
+                        <img src="/images/aivine/second.png" alt="images">
+                    </div>
+                    <div class="home_second_info">
+                        <h4 class="home_second_info_title">A PROBE STYLUS</h4>
+                        <p class="home_second_info_text">Aivine is a box that is 28cm wide and 21.5 in length.</p>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="home_fourt_box">
-            <div class="home_fourt_box_img">
-                <img src="/images/aivine/fourt2.png" alt="images">
+
+        <div class="home_third">
+            <div class="home_third_title">
+                <h2>General view of the AIVINE equipment</h2>
+                <p>PRICE 18.000$</p>
             </div>
-            <div class="home_fourt_box_info">
-                <div class="home_fourt_box_info_text">
-                    <h3>How Many Treatments Does It Take?</h3>
-                    <p>Many hairs will be eliminated with only one treatment, but some will need two or more treatments to achieve permanency. Electrolysis involves a series of treatments over a period of 1-3 months. The overall length of time depends on the amount of hair, its coarseness, and the cause of the excess hair, but once the dermal papilla has been eliminated, the hair is dead and will not grow again.</p>
-                </div>
-                <div class="home_fourt_box_aivine">
-                    <p>- AIVINE Beauty & ESTHETICS -</p>
-                </div>
+            <div class="home_third_image">
+                <img src="/images/aivine/third.png" alt="images">
             </div>
         </div>
-        <div class="home_fourt_box">
-            <div class="home_fourt_box_img">
-                <img src="/images/aivine/fourt2.png" alt="images">
+
+
+        <div class="home_fourth">
+            <div class="home_fourth_title">
+                <h2>We care not only about hair removal, but also your safety, comfort, and beauty.</h2>
             </div>
-            <div class="home_fourt_box_info">
-                <div class="home_fourt_box_info_text">
-                    <h3>How Many Treatments Does It Take?</h3>
-                    <p>Many hairs will be eliminated with only one treatment, but some will need two or more treatments to achieve permanency. Electrolysis involves a series of treatments over a period of 1-3 months. The overall length of time depends on the amount of hair, its coarseness, and the cause of the excess hair, but once the dermal papilla has been eliminated, the hair is dead and will not grow again.</p>
+            <div class="home_fourth_boxes">
+                <div class="home_fourt_box">
+                    <div class="home_fourt_box_img">
+                        <img src="/images/aivine/fourt.png" alt="images">
+                    </div>
+                    <div class="home_fourt_box_info">
+                        <div class="home_fourt_box_info_text">
+                            <h3>How Many Treatments Does It Take?</h3>
+                            <p>Many hairs will be eliminated with only one treatment, but some will need two or more
+                                treatments to achieve permanency. Electrolysis involves a series of treatments over a period
+                                of 1-3 months. The overall length of time depends on the amount of hair, its coarseness, and
+                                the cause of the excess hair, but once the dermal papilla has been eliminated, the hair is
+                                dead and will not grow again.</p>
+                        </div>
+                        <div class="home_fourt_box_aivine">
+                            <p>- AIVINE Beauty & ESTHETICS -</p>
+                        </div>
+                    </div>
                 </div>
-                <div class="home_fourt_box_aivine">
-                    <p>- AIVINE Beauty & ESTHETICS -</p>
+                <div class="home_fourt_box">
+                    <div class="home_fourt_box_img">
+                        <img src="/images/aivine/fourt2.png" alt="images">
+                    </div>
+                    <div class="home_fourt_box_info">
+                        <div class="home_fourt_box_info_text">
+                            <h3>How Many Treatments Does It Take?</h3>
+                            <p>Many hairs will be eliminated with only one treatment, but some will need two or more
+                                treatments to achieve permanency. Electrolysis involves a series of treatments over a period
+                                of 1-3 months. The overall length of time depends on the amount of hair, its coarseness, and
+                                the cause of the excess hair, but once the dermal papilla has been eliminated, the hair is
+                                dead and will not grow again.</p>
+                        </div>
+                        <div class="home_fourt_box_aivine">
+                            <p>- AIVINE Beauty & ESTHETICS -</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="home_fourt_box">
+                    <div class="home_fourt_box_img">
+                        <img src="/images/aivine/fourt2.png" alt="images">
+                    </div>
+                    <div class="home_fourt_box_info">
+                        <div class="home_fourt_box_info_text">
+                            <h3>How Many Treatments Does It Take?</h3>
+                            <p>Many hairs will be eliminated with only one treatment, but some will need two or more
+                                treatments to achieve permanency. Electrolysis involves a series of treatments over a period
+                                of 1-3 months. The overall length of time depends on the amount of hair, its coarseness, and
+                                the cause of the excess hair, but once the dermal papilla has been eliminated, the hair is
+                                dead and will not grow again.</p>
+                        </div>
+                        <div class="home_fourt_box_aivine">
+                            <p>- AIVINE Beauty & ESTHETICS -</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
 
 
 
-<div class="home_sixth">
-    <img src="/images/aivine/sixth.png" alt="images">
-</div>
-
-<div class="home_seventh">
-    <div class="home_seventh_title">
-        <p>By purchasing our equipment, you will become a market-leading professional.</p>
-    </div>
-    <div class="home_seventh_images">
-        <img src="/images/aivine/seventh1.png" alt="images">
-        <img src="/images/aivine/seventh2.png" alt="images">
-    </div>
-</div>
-
-<div class="home_eighth">
-    <div class="home_eighth_title">
-        <p>With Aivine, you provide the maximum for your clients.</p>
-    </div>
-    <div class="home_eighth_info">
-        <div class="home_eighth_boxe">
-            <img src="/images/aivine/eighth1.png" alt="images">
-            <p>Excellent outcome</p>
+        <div class="home_sixth">
+            <img src="/images/aivine/sixth.png" alt="images">
         </div>
-        <div class="home_eighth_boxe">
-            <img src="/images/aivine/eighth2.png" alt="images">
-            <p>Painless procedure</p>
+
+        <div class="home_seventh">
+            <div class="home_seventh_title">
+                <p>By purchasing our equipment, you will become a market-leading professional.</p>
+            </div>
+            <div class="home_seventh_images">
+                <img src="/images/aivine/seventh1.png" alt="images">
+                <img src="/images/aivine/seventh2.png" alt="images">
+            </div>
         </div>
-        <div class="home_eighth_boxe">
-            <img src="/images/aivine/eighth3.png" alt="images">
-            <p>Permanent results</p>
+
+        <div class="home_eighth">
+            <div class="home_eighth_title">
+                <p>With Aivine, you provide the maximum for your clients.</p>
+            </div>
+            <div class="home_eighth_info">
+                <div class="home_eighth_boxe">
+                    <img src="/images/aivine/eighth1.png" alt="images">
+                    <p>Excellent outcome</p>
+                </div>
+                <div class="home_eighth_boxe">
+                    <img src="/images/aivine/eighth2.png" alt="images">
+                    <p>Painless procedure</p>
+                </div>
+                <div class="home_eighth_boxe">
+                    <img src="/images/aivine/eighth3.png" alt="images">
+                    <p>Permanent results</p>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
 
 
 
-<div class="home_ninth">
-    <div class="home_ninth_image">
-        <img src="/images/aivine/ninth1.svg" alt="image">
-        <div class="home_ninth_order_box">
-            <p>We also teach you how to use it for free, both online and offline, in case you purchase.</p>
-            <a href="/contacts">ORDER</a>
-        </div>
-    </div>
-    {{-- <div class="home_ninth_order_box">
+        <div class="home_ninth">
+            <div class="home_ninth_image">
+                <img src="/images/aivine/ninth1.svg" alt="image">
+                <div class="home_ninth_order_box">
+                    <p>We also teach you how to use it for free, both online and offline, in case you purchase.</p>
+                    <a href="/contacts">ORDER</a>
+                </div>
+            </div>
+            {{-- <div class="home_ninth_order_box">
         <p>We also teach you how to use it for free, both online and offline, in case you purchase.</p>
         <a href="/contact">ORDER</a>
     </div> --}}
-</div>
+        </div>
 
 
-<div class="home_tenth">
-    <div class="home_tenth_title">
-        <p>The steps required to complete the order.</p>
+        <div class="home_tenth">
+            <div class="home_tenth_title">
+                <p>The steps required to complete the order.</p>
+            </div>
+            <div class="home_tenth_info">
+                <div class="home_tenth_info_box">
+                    <h3>01</h3>
+                    <p>Fill in your information.</p>
+                </div>
+                <div class="home_tenth_info_box">
+                    <h3>02</h3>
+                    <p>Please wait for our call.</p>
+                </div>
+                <div class="home_tenth_info_box">
+                    <h3>03</h3>
+                    <p>Within a few days, you will obtain your Aivine.</p>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="home_tenth_info">
-        <div class="home_tenth_info_box">
-            <h3>01</h3>
-            <p>Fill in your information.</p>
-        </div>
-        <div class="home_tenth_info_box">
-            <h3>02</h3>
-            <p>Please wait for our call.</p>
-        </div>
-        <div class="home_tenth_info_box">
-            <h3>03</h3>
-            <p>Within a few days, you will obtain your Aivine.</p>
-        </div>
-    </div>
-</div>
-
-
 @endsection
 @section('script')
 @endsection
