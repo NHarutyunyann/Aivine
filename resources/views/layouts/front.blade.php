a8dcea3040a7fecb4147da24850e022499c112a6
N-Harutyunayn <12.Harutyunyan@mail.ru>
2024-09-01 14:38:38 +0400

    das

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="google-site-verification" content="4KP0mHB-RKmQrJzLm98WkbdV-VDtF5LQeJvx1CVjkLA" />
-    <link rel="icon" type="image/x-icon" href="/images/aivine/Logo.png">
+    <link rel="icon" type="image/x-icon" href="/images/aivine/logo.png">
     <title> @yield('title')</title>
     <meta name="description" content=" @yield('description')">
     <meta name="keywords" content=" @yield('keywords')">
